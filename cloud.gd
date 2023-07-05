98d2d0852035215380cab342eba11ed9a0708940
SeukWon Kang <kasworld@gmail.com>
2023-07-05 15:59:55 +0900

    change cloud

--- cloud.gd
+++ cloud.gd
@@ -6,10 +6,13 @@ var velocity :Vector2
 func _ready() -> void:
 	var vp = get_viewport_rect().size
 	position = Vector2(randf_range(0,vp.x),randf_range(0,vp.y))
-	$CloudSprites.frame = randi_range(0,3)
 	velocity = Vector2.ONE.rotated( randf() * 2 * PI ) * SPEED_LIMIT
+	rotate(randf()*2*PI)
 	if randi_range(0,1) == 0 :
-		$CloudSprites.flip_h = true
+		$Sprite2D.flip_h = true
+	if randi_range(0,1) == 0 :
+		$Sprite2D.flip_v = true
+	$Sprite2D.self_modulate.a = randf()
 
 func _physics_process(delta: float) -> void:
 	var r = $CollisionShape2D.shape.radius
