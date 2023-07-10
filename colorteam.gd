00d5fd7a0b0617d85511c9a24e3af1f3ccfd12e8
SeukWon Kang <kasworld@gmail.com>
2023-07-10 17:15:21 +0900

    prevent same color team, limit retry

--- colorteam.gd
+++ colorteam.gd
@@ -5,11 +5,23 @@ var color :Color
 var name :String
 
 static func make_color_teamlist(team_count :int)->Array[ColorTeam]:
+	var in_use_index = {}
 	var rtn :Array[ColorTeam]
-	var color_count =NamedColorList.color_list.size()
+	var color_count = NamedColorList.color_list.size()
 	for t in team_count:
 		var ct = ColorTeam.new()
-		ct.color_index = randi() % color_count
+		var try_color_index :int
+		var try_count = 10
+		while true:
+			try_color_index = randi() % color_count
+			if in_use_index.get(try_color_index) == null :
+				break
+			try_count -= 1
+			if try_count < 0:
+				print("too many retry")
+				break
+		in_use_index[try_color_index] = true
+		ct.color_index = try_color_index
 		ct.name = NamedColorList.get_colorname(ct.color_index)
 		ct.color = NamedColorList.get_color(ct.color_index)
 		rtn.append(ct)
