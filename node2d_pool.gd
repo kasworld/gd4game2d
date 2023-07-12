f9b0c4e024bfb5ea03a48b10a370cdbd2aa92842
SeukWon Kang <kasworld@gmail.com>
2023-07-12 10:51:56 +0900

    update freelist

--- node2d_pool.gd
+++ node2d_pool.gd
@@ -2,13 +2,22 @@ class_name Node2DPool
 
 var pool := []
 var instantiatefn : Callable
+var total_count :int
+var reuse_count :int
+var make_count :int
+
+func hit_rate()->float:
+	return float(reuse_count)/float(total_count)
 
 func _init(newfn :Callable) -> void:
 	instantiatefn = newfn
 
 func get_node2d()->Node2D:
+	total_count +=1
 	if pool.size() > 0 :
+		reuse_count +=1
 		return pool.pop_back()
+	make_count +=1
 	return instantiatefn.call()
 
 func put_node2d( o :Node2D):
