cfdcd3ce0f6b3c2c98cedd8e1486dd59c08264a6
SeukWon Kang <kasworld@gmail.com>
2023-07-12 17:34:16 +0900

    update colorteam

--- colorteam.gd
+++ colorteam.gd
@@ -4,12 +4,16 @@ var color_index :int
 var color :Color
 var name :String
 
+func _init(ci :int):
+	color_index = ci
+	color = NamedColorList.get_color(color_index)
+	name = NamedColorList.get_colorname(color_index)
+
 static func make_color_teamlist(team_count :int)->Array[ColorTeam]:
 	var in_use_index = {}
 	var rtn :Array[ColorTeam] = []
 	var color_count = NamedColorList.color_list.size()
 	for t in team_count:
-		var ct = ColorTeam.new()
 		var try_color_index :int
 		var try_count = 10
 		while true:
@@ -22,9 +26,7 @@ static func make_color_teamlist(team_count :int)->Array[ColorTeam]:
 				print_debug("too many retry")
 				break
 		in_use_index[try_color_index] = true
-		ct.color_index = try_color_index
-		ct.name = NamedColorList.get_colorname(ct.color_index)
-		ct.color = NamedColorList.get_color(ct.color_index)
+		var ct = ColorTeam.new(try_color_index)
 		rtn.append(ct)
 #		print("%s %s %s %s" % [t, ct.color_index, ct.color, ct.name])
 	return rtn
